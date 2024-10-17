c511d8dea9737731cc77506ea082a8c5631dc8a8
Nadia Yakta <nadia.yakta@learnet.se>
2024-10-17 10:25:23 +0200

    prefix and bubble makes sound

--- Assets/Scripts/Clickable.cs
+++ Assets/Scripts/Clickable.cs
@@ -3,8 +3,12 @@ using UnityEngine.EventSystems;
 
 public class Clickable : MonoBehaviour, IPointerClickHandler
 {
+    [SerializeField] private AudioSource _sfxBubble;
+
     public void OnPointerClick(PointerEventData eventData)
     {
         GameManager.Singleton.OnClick();
+
+        _sfxBubble.Play();
     }
 }
